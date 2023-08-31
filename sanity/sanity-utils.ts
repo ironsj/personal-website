0c6a534151c4d65bb27e3f9744043ba59645062c
ironsj <jakeirons007@outlook.com>
2023-08-30 23:50:49 -0400

    Adding posts to home page to test

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,3 +1,21 @@
+import { createClient, groq } from "next-sanity";
+
 export async function getPosts(){
-    
+    const client = createClient({
+        projectId: "zo1e4sf7",
+        dataset: "production",
+        apiVersion: "2023-08-29",
+    });
+
+    return client.fetch(
+        groq`*[_type == "post"]| order(_createdAt asc){
+            _id,
+            _createdAt,
+            title,
+            "slug": slug.current,
+            publishedAt,
+            "image": mainImage.asset->url,
+            body,
+        }`
+    )
 }
